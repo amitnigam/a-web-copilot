0106bd935dceb6a66d9db5e9ebacb09f860aba7d
amitnigam <amitnigam.in@gmail.com>
2026-03-17 11:17:13 -0400

    added post hog

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,20 @@
+import posthog from "posthog-js";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+
+// Initialize PostHog analytics + session replay
+if (typeof window !== "undefined") {
+	posthog.init("phc_SS2Fg8CYo9IaZeiDMz71k9Pz0aY39rc4RkLF5xOIirt", {
+		api_host: "https://us.i.posthog.com",
+		capture_pageview: true,
+		capture_pageleave: true,
+		session_recording: {
+			maskAllInputs: false,
+		},
+		// Provided default from PostHog snippet
+		defaults: "2026-01-30",
+	});
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
